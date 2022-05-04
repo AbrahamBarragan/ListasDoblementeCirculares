dbcb81cc6631fdfd030330a2fd4cbf4b2393281f
Abraham Barragan Lira <bala.barragan3@hotmail.com>
2022-05-03 18:51:50 -0700

    Agregar archivos de proyecto.

---
+++ ListasDoblementeCirculares/Nodo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDoblementeCirculares
+{
+    public class Nodo
+    {
+        public string Valor { get; set; }
+        public Nodo nodoSiguiente { get; set; }
+        public Nodo nodoAnterior { get; set; }
+        public Nodo(string valor = "", Nodo nodoanterior = null, Nodo nodosiguiente = null)
+        {
+            Valor = valor;
+            nodoAnterior = nodoanterior;
+            nodoSiguiente = nodosiguiente;
+        }
+    }
+}
---
+++ ListasDoblementeCirculares/Program.cs
@@ -0,0 +1,15 @@
+﻿using ListasDoblementeLigadas;
+
+Listas lista = new Listas();
+lista.AgregarNodo("Uno");
+lista.AgregarNodo("Dos");
+lista.AgregarNodo("Tres");
+lista.AgregarNodo("Cuatro");
+lista.AgregarNodo("Cinco");
+Console.WriteLine(lista.RecorrerLista());
+Console.WriteLine("Buscando nodo Cuatro");
+Nodo nodoCuatro = lista.Buscar("Cuatro");
+Console.WriteLine(nodoCuatro.Valor);
+Console.WriteLine("Borrando Cuatro");
+lista.BorrarNodo("Cuatro");
+Console.WriteLine(lista.RecorrerLista());
